7e8254b4a9024470525182f4a7f58de18c61b3ef
Dat19022001 <nguyenthedat1902@gmail.com>
2022-02-17 11:25:59 +0700

    update

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$('.mobile__sub-right').slick({
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slide-arrow prev-arrow"><i style="position:relative; width: 36px;line-height: 36px;top:0;right:0" class="fa fa-angle-left"></i></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"><i style="position:relative; width: 36px;line-height: 36px;top:0;right:0" class="fa fa-angle-right"></i></button>'
+  });
